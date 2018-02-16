50273c355d837f540ed137674afc70bd768dfe03
Brandon Morse <brandon@morsecodemedia.com>
2018-02-16 14:10:51 -0500

    wrote custom extend method to removie use of jQuery extend

--- main.js
+++ main.js
@@ -10,71 +10,86 @@
 		"longWordDelay": 1.4
 	};
 
+	function transferComplete() {
+		if (this.status === 200) {
+			playReadContent(this.responseText);
+		} else {
+			var text = '';
+			var elements = $('p, li, h1, h2, h3, h4, h5, h6, span, pre');
+			elements.each(function(index, element) {
+				element = $(element);
+				var elementText = element
+					.clone()
+					.children('sup')
+					.remove()
+					.end()
+					.text()
+					.trim();
+				if (elementText.length >= 60)
+					if (!(element.tagName === 'LI' && elementText.includes('    ')))
+						text += " " + elementText;
+			});
+			playReadContent(text);
+		}
+	}
+	function transferFailed(e) {
+		console.log('Caught Exception: ' + e.description);
+	}
+
 	chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
 		switch (request.functiontoInvoke) {
 			case "readSelectedText":
 				playReadContent( request.selectedText );
 				break;
 			case "readFullPage":
-				var getArticle = $.get( 'https://readparser.herokuapp.com/?url=' + document.URL );
-				getArticle.success(function( result ) {
-					playReadContent( result );
-				}).error(function( jqXHR, textStatus, errorThrown ) {
-					var text = '';
-					var elements = $('p, li, h1, h2, h3, h4, h5, h6, span, pre');
-					elements.each(function(index, element) {
-						element = $(element);
-						var elementText = element
-							.clone()
-							.children('sup')
-							.remove()
-							.end()
-							.text()
-							.trim();
-						if (elementText.length >= 60)
-							if (!(element.tagName === 'LI' && elementText.includes('    ')))
-								text += " " + elementText;
-					});
-					playReadContent(text);
-				});
+				try {
+					var httpRequest = new XMLHttpRequest();
+					httpRequest.open('GET', 'https://readparser.herokuapp.com/?url=' + document.URL, true);
+					httpRequest.addEventListener("load", transferComplete);
+					httpRequest.addEventListener("error", transferFailed);
+					httpRequest.send();
+
+				} catch(e) {
+					transferFailed(e);
+				}
 				break;
 			default:
 				break;
 		}
 	});
 
-	$(document).on( 'blur', '.__read .__read_speed', function () {
-		var val = Math.min( 15000, Math.max( 0, parseInt(this.value,10)));
-		setReadOptions( {"wpm": val} );
-	});
+	// $(document).on( 'blur', '.__read .__read_speed', function () {
+	// 	var val = Math.min( 15000, Math.max( 0, parseInt(this.value,10)));
+	// 	setReadOptions( {"wpm": val} );
+	// });
 
-	$(document).on( 'blur', '.__read .__read_slow_start', function () {
-		var val = Math.min( 5, Math.max( 1, parseInt(this.value,10)));
-		setReadOptions( {"slowStartCount": val} );
-	});
+	// $(document).on( 'blur', '.__read .__read_slow_start', function () {
+	// 	var val = Math.min( 5, Math.max( 1, parseInt(this.value,10)));
+	// 	setReadOptions( {"slowStartCount": val} );
+	// });
 
-	$(document).on( 'blur', '.__read .__read_sentence_delay', function () {
-		var val = Math.min( 5, Math.max( 0, Number(this.value)));
-		setReadOptions( {"sentenceDelay": val} );
-	});
+	// $(document).on( 'blur', '.__read .__read_sentence_delay', function () {
+	// 	var val = Math.min( 5, Math.max( 0, Number(this.value)));
+	// 	setReadOptions( {"sentenceDelay": val} );
+	// });
 
-	$(document).on( 'blur', '.__read .__read_punc_delay', function () {
-		var val = Math.min( 5, Math.max( 0, Number(this.value)));
-		setReadOptions( {"otherPuncDelay": val} );
-	});
+	// $(document).on( 'blur', '.__read .__read_punc_delay', function () {
+	// 	var val = Math.min( 5, Math.max( 0, Number(this.value)));
+	// 	setReadOptions( {"otherPuncDelay": val} );
+	// });
 
-	$(document).on( 'blur', '.__read .__read_short_word_delay', function () {
-		var val = Math.min( 5, Math.max( 0, Number(this.value)));
-		setReadOptions( {"shortWordDelay": val} );
-	});
+	// $(document).on( 'blur', '.__read .__read_short_word_delay', function () {
+	// 	var val = Math.min( 5, Math.max( 0, Number(this.value)));
+	// 	setReadOptions( {"shortWordDelay": val} );
+	// });
 
-	$(document).on( 'blur', '.__read .__read_long_word_delay', function () {
-		var val = Math.min( 5, Math.max( 0, Number(this.value)));
-		setReadOptions( {"longWordDelay": val} );
-	});
+	// $(document).on( 'blur', '.__read .__read_long_word_delay', function () {
+	// 	var val = Math.min( 5, Math.max( 0, Number(this.value)));
+	// 	setReadOptions( {"longWordDelay": val} );
+	// });
 
 	function setReadOptions ( myOptions ) {
-		readOptions = $.extend( {}, readOptions, myOptions );
+		readOptions = extend( readOptions, myOptions );
 		chrome.storage.sync.clear(function () {
 			chrome.storage.sync.set(readOptions, function() {
 				//console.log('[READ] set:', readOptions);
@@ -84,11 +99,22 @@
 
 	function playReadContent ( text ) {
 		chrome.storage.sync.get(null, function ( myOptions ) {
-			readOptions = $.extend( {}, readOptions, myOptions );
+			readOptions = extend( readOptions, myOptions );
 			r = new Read ( readOptions );
 			r.setText(text);
 			r.play();
 		});
+	}
+
+	function extend(ogObj, newObj) {
+		var returnObj = {}
+		for( var s in ogObj ) {
+			returnObj[s] = ogObj[s];
+		}
+		for( s in newObj){
+			returnObj[s] = newObj[s];
+		}
+		return returnObj;
 	}
 
 })();
